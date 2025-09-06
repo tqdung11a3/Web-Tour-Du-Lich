30b1ab4eeffa8b43d12dc900c691c31787f80a5a
tqdung11a3 <truongquocdung2004@gmail.com>
2025-09-06 16:06:35 +0700

    update register page

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const accountController = require("../../controllers/admin/account.controller");
 
 router.get("/login", accountController.login);
+router.get("/register", accountController.register);
 
 module.exports = router;
